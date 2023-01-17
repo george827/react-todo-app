88c39e302280d97dcd4fc8b67b17f4c8c73433fd
george827 <kinyanjuigeorge827@gmail.com>
2023-01-17 15:10:27 +0300

    delete to do

---
+++ src/components/TodoItem.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+// style sheet module
+import styles from "./TodoItem.module.css"
+
+class TodoItem extends React.Component {
+    handleEditing = ()=> {
+        console.log("edit mode activated")
+    }
+    render(){
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#595959",
+            opacity: 0.4,
+            textDecoration: "line-through",
+          }
+          const { completed, id, title } = this.props.todo
+        return (
+            <li className={styles.item}>
+                <div onDoubleClick={this.handleEditing}>
+                    <input 
+                        type="checkbox"
+                        className={styles.checkbox}
+                        checked={completed}
+                        onChange={()=> this.props.handleChangeProps(id)}
+                    /> 
+                    <button onClick={()=> this.props.deleteTodoProps(id)}>
+                        Delete
+                    </button>
+                    <span style={completed ? completedStyle : null}>{title}</span>
+                </div>
+                <input type="text" className={styles.textInput} />
+            </li>
+        )
+    }
+    
+}
+
+export default TodoItem
